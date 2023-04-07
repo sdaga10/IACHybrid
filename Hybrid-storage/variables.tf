ba39a381c0ece11b7551e9d1a58033e9edf27be9
sdaga10 <shilpa.daga89@gmail.com>
2023-04-07 15:54:17 +0530

    Update variables.tf

--- Hybrid-storage/variables.tf
+++ Hybrid-storage/variables.tf
@@ -1,4 +1,4 @@
-/*
+
 variable "aws_access_key" {
   type = string
   default = "****"
@@ -8,25 +8,13 @@ variable "aws_secret_key" {
   type = string
   default = "****"
 }
-*/
-
-variable "aws_access_key" {
-  type = string
-  default = "AKIAX6R6O2DQZUIQ4OZL"
-}
-variable "aws_secret_key" {
-  type = string
-  default = "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"
-}
-
-
 
 
 variable "azure_subscription_id" {
   type = string
-  default = "a3b0c3f1-86a7-4d6e-926f-6e09db85c4aa"
+  default = "****"
 }
-/*
+
 variable "azure_client_id" {
   type = string
   default = "****"
@@ -41,7 +29,7 @@ variable "azure_tenant_id" {
   type = string
   default = "****"
 }
-*/
+
 variable "aws_region" {
   type = string
   default = "us-west-2"
